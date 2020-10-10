f92421e67f449127ce02ec6389b6c87587134ee6
Pavel <pavel.d.orel@gmail.com>
2020-10-10 17:04:09 -0400

    Initial Commit

--- Nearby/Supporting Files/AppDelegate.swift
+++ Nearby/Supporting Files/AppDelegate.swift
@@ -1,17 +1,7 @@
-//
-//  AppDelegate.swift
-//  Nearby
-//
-//  Created by Pavel Orel on 2020-10-10.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
