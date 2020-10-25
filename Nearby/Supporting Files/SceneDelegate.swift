367bcf604ffb5c6c9d9f0d6fd0ac23b1f28edd6a
Pavel <pavel.d.orel@gmail.com>
2020-10-24 23:38:39 -0400

    Start browsing on launch from SceneDelegate

--- Nearby/Supporting Files/SceneDelegate.swift
+++ Nearby/Supporting Files/SceneDelegate.swift
@@ -2,30 +2,32 @@ import UIKit
 import ReSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+  
   var window: UIWindow?
-
+  
   func scene(
     _ scene: UIScene,
     willConnectTo session: UISceneSession,
     options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = scene as? UIWindowScene else { return }
-        
+    
+    Inject.ChatManager().setUpAndStartDiscovery()
+    
     let newWindow = UIWindow(windowScene: windowScene)
     newWindow.rootViewController = UINavigationController(rootViewController: ChatBrowserController())
     
     newWindow.makeKeyAndVisible()
     window = newWindow
   }
-
+  
   func sceneDidDisconnect(_ scene: UIScene) { }
-
+  
   func sceneDidBecomeActive(_ scene: UIScene) { }
-
+  
   func sceneWillResignActive(_ scene: UIScene) { }
-
+  
   func sceneWillEnterForeground(_ scene: UIScene) { }
-
+  
   func sceneDidEnterBackground(_ scene: UIScene) { }
 }
 
